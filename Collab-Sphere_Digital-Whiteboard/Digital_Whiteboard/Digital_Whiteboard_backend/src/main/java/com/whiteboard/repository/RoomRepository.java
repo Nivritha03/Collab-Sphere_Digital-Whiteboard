94a7c66a6fff0b84aacc05a20acd47aa625109f2
Nivritha Pola <nivritha.pola@gmail.com>
2026-04-01 00:22:43 +0530

    clean project structure (backend + frontend)

--- Collab-Sphere_Digital-Whiteboard/Digital_Whiteboard/Digital_Whiteboard_backend/src/main/java/com/whiteboard/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.whiteboard.repository;
-
-import com.whiteboard.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, String> {
-}
--- Collab-Sphere_Digital-Whiteboard/Digital_Whiteboard/Digital_Whiteboard_backend/src/main/java/com/whiteboard/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.whiteboard.repository;
-
-import com.whiteboard.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
